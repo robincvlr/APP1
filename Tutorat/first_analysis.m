19f1950adc119c299eaa7a70e74ecc15900abaa0
Default User <user@debian.(none)>
2017-10-05 10:04:36 -0400

    ajout des points et début algo

--- Tutorat/first_analysis.m
+++ Tutorat/first_analysis.m
@@ -1,3 +1,10 @@
-function first_analysis
+#Par Robin Cavalieri
+function first_analysis 
+  #Création des vecteur de données (P300)
+  p_300 = load("/Points/C1.txt");
+  p_300_opp = load("/Points/C2.txt")
+  #connaitre la taille du vecteur de données
+  size_datas[i,j] = size(datas)
+  #calcul de la moyenne 
   
 endfunction
