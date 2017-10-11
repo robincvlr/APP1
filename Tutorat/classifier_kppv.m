1ecee4f268d34e996db5ff9d33e6e0131c34a46a
zakariabelgoum <zakariabelgoum94@gmail.com>
2017-10-11 17:40:56 -0400

    zakaria

---
+++ Tutorat/classifier_kppv.m
@@ -0,0 +1,5 @@
+function classifier_kppv
+ref_P300 = load("Donnees/ref_P300")
+ref_P300=red_matrice(ref_P300, 3);
+disp(ref_P300);
+endfunction
---
+++ Tutorat/test_kppv.m
@@ -0,0 +1,2 @@
+C=Classifier_kppv(1,0.213);
+disp(C);
